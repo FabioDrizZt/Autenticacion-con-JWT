88bd7e7172163301daeb3a5632ba3b37980e6983
fabio <fabio>
2024-06-13 19:52:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+process.loadEnvFile()
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
